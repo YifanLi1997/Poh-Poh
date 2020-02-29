6f62263bea9e83a50c1aca5ca0266703efc22bdc
李一帆 <liyifan@liyifandeMacBook-Pro.local>
2020-03-01 03:02:01 +0800

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float horizontalForceFactor = 5;
+
+    Rigidbody m_rigidbody;
+
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_rigidbody.AddRelativeForce(Vector3.up * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * horizontalForceFactor * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * horizontalForceFactor * Time.deltaTime);
+        }
+    }
+}
